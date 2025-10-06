1711a8a8db08b7c2c90d4d6bdf6475ae2384c8f3
gandharvak <gandharvakulkarni2003@gmail.com>
2025-10-06 13:26:40 +0530

    minor: team member sequence

--- src/assets/team/index.ts
+++ src/assets/team/index.ts
@@ -1,15 +1,9 @@
 export const teamMembers = [
-    {
+  {
     name: "Shrilekha",
     role: "Creative Director",
     photo: "https://blogger.googleusercontent.com/img/a/AVvXsEhOTZUDOL5ZedyNhmVAP1FMI-CO7QWgvwV4_4YTUFj5Slsje3m9xxncc0g-fb1Sp4-1lGATq1iKM_Ala7iwL7hnL2lDG-MOpk6xIu7qhJ_RE2UMC9kkXDARzU7mzp4d-NwgT3--VH1_FKq6u2GW_c3hz5s16GG8nHU148mxh-hfLYRQ5zFvTRX6yKfdAn4c",
     bio: "Shrilekha is the creative force that drives our projects forward. As a creative director, she oversees the artistic vision and ensures that every element aligns with the story we aim to tell. Her innovative ideas and meticulous attention to detail elevate our work to new heights.",
-  },
-  {
-    name: "Anil",
-    role: "Documentary Filmmaker",
-    photo: "https://blogger.googleusercontent.com/img/a/AVvXsEiHWkFbhjxM3A7kVO15WSCLJheP7hlfZ4DCKpQSJfosIxHd3n7NtNjPTthTD8uLg2hhGztQVb3iyXUk340ez8O8EC8thWnbUmU-_7Fsd3zEaLaTcY7wmMRyevu4ZMdqciras8BUqRPbQDGBZG_RJRIBUkH6S215YZgOV3tBp2b9TUdo-GV1W5_iIiPANEsM",
-    bio: "Anil brings stories to life through his lens. With a background in documentary filmmaking, he has a unique ability to capture raw emotions and candid moments. His work has been featured in various international film festivals, earning him accolades for his storytelling prowess.",
   },
   {
     name: "Harshad",
@@ -18,10 +12,28 @@ export const teamMembers = [
     bio: "Harshad is the wizard behind the scenes, transforming raw footage into cinematic masterpieces. His expertise in post-production and drone cinematography adds a dynamic dimension to our storytelling, ensuring every project is visually stunning and emotionally resonant.",
   },
   {
+    name: "Sachin",
+    role: "Real Shot Artist",
+    photo: "https://blogger.googleusercontent.com/img/a/AVvXsEg9F9Fl50wXKn59TaGRpsxYze2cyktjzyxodlEYa9e2Yuht7W4FOfO-U7AWEGgcgGuwSftXV1HLkqB8yixVtC8FufvNfBqYGrW-bVu2ieR81QvqYKcFSNCOuTmSa0GCzJd7ycRyAZ-LPsQTxxIGch6yj5vVZG2rNMzO3wSS1_1pFWCXWS58C_hBqNjtJbtB",
+    bio: "Sachin is a talented real shot artist who excels in capturing spontaneous and genuine moments. His ability to anticipate and frame these fleeting instances results in photographs that are both lively and heartfelt, making him an invaluable member of our team.",
+  },
+  {
     name: "Prathamesh",
     role: "Visual Storyteller",
     photo: "https://blogger.googleusercontent.com/img/a/AVvXsEgWUlwr9DVY4a8nEhDAc7zFKjWECq_ZBthoJHSWVsV1Zwv5WmHWYOLQWreSNgc-zE_jLFym37nnO4_KPnmiYCvABYVpnXPLYOdMlCXtONeQBQMOtnZPDgb4Nau3kbA_6tBTz_gbIM0iaJLGNkF5th-kmfGeCG0Wrov2aZJOecQmTk2iYaX6OwDgX_uicGxr",
     bio: "Prathamesh has a knack for weaving narratives through his photography. His ability to connect with subjects and capture their essence results in images that are both powerful and evocative. He specializes in creating visual stories that resonate deeply with viewers.",
+  },
+  {
+    name: "Vaibhav",
+    role: "Visual Storyteller",
+    photo: "https://blogger.googleusercontent.com/img/a/AVvXsEh6ACuH0QPb1A5ApURR3r4-npq8Qft2DTQWv3vLp1ISr4eBVicjidq5mznPGr4LdZO24JSUPP1iUDwWEA7GzaekhfknsORwRWMLf8G4rITK3nrV-5aQ3vhgg0GRd4OFYOp6974rD2GT-8OEuEo1kjU4s_sMVER2NC_N88iEjZogOOKtDaXQxcg-j7Hmxyf2",
+    bio: "Vaibhav excels at crafting compelling narratives through his lens. His keen eye and creative approach allow him to capture moments that speak volumes, creating images that inspire and engage. He specializes in telling stories that leave a lasting impression on viewers.",
+  },
+  {
+    name: "Anil",
+    role: "Documentary Filmmaker",
+    photo: "https://blogger.googleusercontent.com/img/a/AVvXsEiHWkFbhjxM3A7kVO15WSCLJheP7hlfZ4DCKpQSJfosIxHd3n7NtNjPTthTD8uLg2hhGztQVb3iyXUk340ez8O8EC8thWnbUmU-_7Fsd3zEaLaTcY7wmMRyevu4ZMdqciras8BUqRPbQDGBZG_RJRIBUkH6S215YZgOV3tBp2b9TUdo-GV1W5_iIiPANEsM",
+    bio: "Anil brings stories to life through his lens. With a background in documentary filmmaking, he has a unique ability to capture raw emotions and candid moments. His work has been featured in various international film festivals, earning him accolades for his storytelling prowess.",
   },
   {
     name: "Sachin",
@@ -29,16 +41,4 @@ export const teamMembers = [
     photo: "https://blogger.googleusercontent.com/img/a/AVvXsEhmzDC3qHMb-aJfj5imDmgwy0cE9Sro6B5jh4FkCbsZel5Mo0SXHIfUkCJD0xsLLX872YGaGvAwAkVTzxtCwbzmc8geV1vJ61Lnq3jobetClvXGxWeWjUpcYzShyknbvsNvXo_uqdvhDN6FiVLgby_Xp9UTLl6rvBwsxY_aqVgmqM72U7iCcu9CvX6KAXh_",
     bio: "Sachin is a seasoned documentary filmmaker with a passion for storytelling. His work focuses on real-life stories that inspire and provoke thought. With a keen eye for detail and a compassionate approach, he brings authenticity and depth to every project he undertakes.",
   },
-  {
-    name: "Sachin",
-    role: "Real Shot Artist",
-    photo: "https://blogger.googleusercontent.com/img/a/AVvXsEg9F9Fl50wXKn59TaGRpsxYze2cyktjzyxodlEYa9e2Yuht7W4FOfO-U7AWEGgcgGuwSftXV1HLkqB8yixVtC8FufvNfBqYGrW-bVu2ieR81QvqYKcFSNCOuTmSa0GCzJd7ycRyAZ-LPsQTxxIGch6yj5vVZG2rNMzO3wSS1_1pFWCXWS58C_hBqNjtJbtB",
-    bio: "Sachin is a talented real shot artist who excels in capturing spontaneous and genuine moments. His ability to anticipate and frame these fleeting instances results in photographs that are both lively and heartfelt, making him an invaluable member of our team.",
-  },
- {
-  name: "Vaibhav",
-  role: "Visual Storyteller",
-  photo: "https://blogger.googleusercontent.com/img/a/AVvXsEh6ACuH0QPb1A5ApURR3r4-npq8Qft2DTQWv3vLp1ISr4eBVicjidq5mznPGr4LdZO24JSUPP1iUDwWEA7GzaekhfknsORwRWMLf8G4rITK3nrV-5aQ3vhgg0GRd4OFYOp6974rD2GT-8OEuEo1kjU4s_sMVER2NC_N88iEjZogOOKtDaXQxcg-j7Hmxyf2",
-  bio: "Vaibhav excels at crafting compelling narratives through his lens. His keen eye and creative approach allow him to capture moments that speak volumes, creating images that inspire and engage. He specializes in telling stories that leave a lasting impression on viewers.",
-} 
 ];
